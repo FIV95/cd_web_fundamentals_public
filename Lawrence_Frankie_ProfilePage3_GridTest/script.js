f986274b3261ddad4ecdede51b1015b4475d2e7c
Frankie Lawrence <frank4law@gmail.com>
2023-10-12 00:00:48 -0400

    Javascript Changes - still broken

--- Lawrence_Frankie_ProfilePage3_GridTest/script.js
+++ Lawrence_Frankie_ProfilePage3_GridTest/script.js
@@ -1,13 +1,13 @@
 const userName = document.getElementsByClassName('Name')[0]
 const edit = document.getElementById('setting')
 const edit2 = document.getElementById('gearIcon')
-const userdata = document.querySelector('.column2')
+const userdata = document.querySelector('.connnectionRequsts')
 // const user1 = [document.querySelector('#toddImg'),document.getElementById('todd'),userdata.querySelector('.yesNoButton')];
 const user1Image = document.getElementById('toddImg');
 const user1Name = document.getElementById('todd');
 const acceptDeclineButtons = document.querySelector('.acceptDeclineButtons')
-const user1Yes = acceptDeclineButtons.querySelector('.yesNoButton');
-const user1No = acceptDeclineButtons.querySelectorAll('.yesNoButton')[1]
+const user2Image = document.getElementById('philImg');
+const User2Name = document.getElementById('phil');
 
 let editProfile = () => {
       console.log(userName.innerText);
@@ -22,6 +22,11 @@ userdata.addEventListener('click', function (event) {
       if (target.classList.contains('yesNoButton')) {
             const user = target.getAttribute('data-user');
             if (user === 'user1') {
+                  const acceptDeclineButtons = target.closest('.acceptDeclineButtons');
+                  if (acceptDeclineButtons) {
+                        acceptDeclineButtons.remove()
+                  }
+            } if (user === 'user2'){
                   const acceptDeclineButtons = target.closest('.acceptDeclineButtons');
                   if (acceptDeclineButtons) {
                         acceptDeclineButtons.remove()
